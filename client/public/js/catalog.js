f1e9c277b101e2d675ac8889a6b6a0e8d16d0d8b
Me <maximborsiev@gmail.com>
2024-05-19 22:10:53 +0200

    .

--- client/public/js/catalog.js
+++ client/public/js/catalog.js
@@ -135,7 +135,6 @@ function updateBook(uuid, book) {
         });
 }
 
-
 function truncateText(text, maxLength) {
     return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
 }
