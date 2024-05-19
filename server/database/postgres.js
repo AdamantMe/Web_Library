b5747c03b0e092d57cdc769b76ced318c12377fe
Me <maximborsiev@gmail.com>
2024-05-19 21:22:51 +0200

    deployment updates

--- server/database/postgres.js
+++ server/database/postgres.js
@@ -2,7 +2,18 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const { Pool } = require('pg');
 
-const pool = new Pool(JSON.parse(process.env.POSTGRES));
+let pool;
+
+if (process.env.NODE_ENV === 'production') {
+    pool = new Pool({
+        connectionString: process.env.POSTGRES,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    pool = new Pool(JSON.parse(process.env.POSTGRES));
+}
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
