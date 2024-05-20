e621a4ae0a6095e0f059a1d624136938f41de353
Me <31238048+AdamantMe@users.noreply.github.com>
2024-05-20 21:22:19 +0200

    minor error fix

--- client/public/js/catalog.js
+++ client/public/js/catalog.js
@@ -24,19 +24,10 @@ function fetchAllBooks() {
     const searchQuery = document.getElementById('search-input').value.toLowerCase();
 
     fetch(`${API_BASE_URL}/books`, { headers: getHeaders() })
-        .then(response => {
-            if (response.status === 401) {
-                alert('You need to log in to view this section.');
-                window.location.href = '/html/login.html';
-                return;
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-            if (data) {
-                allBooks = data.filter(book => book.title.toLowerCase().includes(searchQuery));
-                filterBooks();
-            }
+            allBooks = data.filter(book => book.title.toLowerCase().includes(searchQuery));
+            filterBooks();
         })
         .catch(error => console.error('Error fetching books:', error));
 }
@@ -119,11 +110,6 @@ function updateBook(uuid, book) {
         body: JSON.stringify(book)
     })
         .then(response => {
-            if (response.status === 401) {
-                alert('You need to log in to update this book.');
-                window.location.href = '/html/login.html';
-                return;
-            }
             if (!response.ok) {
                 return response.json().then(errorData => {
                     throw new Error(errorData.message || 'Error updating book');
