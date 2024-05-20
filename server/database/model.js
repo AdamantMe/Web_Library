465c726efd7ec65a1dc06c84cde26beab8eaa595
Me <31238048+AdamantMe@users.noreply.github.com>
2024-05-20 22:09:19 +0200

    some cleanup

--- server/database/model.js
+++
@@ -1,9 +0,0 @@
-const db = require('./postgres');
-
-module.exports = {
-    async getUsers() {
-        const sql = `SELECT * FROM users`;
-        const result = await db.query(sql);
-        return result.rows;
-    }
-};
