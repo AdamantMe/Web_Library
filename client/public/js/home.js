a0dced6b9c88e35974ce449d64c457b3ef2c61bb
Me <maximborsiev@gmail.com>
2024-05-24 19:20:47 +0200

    Added modal

--- client/public/js/home.js
+++ client/public/js/home.js
@@ -12,29 +12,44 @@ function fetchBooksForSections() {
 
 function fetchBooksForSection(section, endpoint) {
     fetch(`${API_BASE_URL}${endpoint}`, { headers: getHeaders() })
-    .then(response => {
-        if (response.status === 401) {
-            alert('You need to log in to view this section.');
-            window.location.href = '/html/login.html';
-            return;
-        }
-        return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
-        if (data) {
-            const sectionList = document.getElementById(section);
-            sectionList.innerHTML = '';
-            data.forEach((book, index) => {
-                const bookElement = document.createElement('div');
-                bookElement.className = 'item';
-                const uniqueParam = new Date().getTime() + index;
-                bookElement.innerHTML = `
-                    <img src="${book.title_image}?random=${uniqueParam}" alt="${book.title}">
-                    <h3>${book.title}</h3>
-                `;
-                sectionList.appendChild(bookElement);
-            });
-        }
+        const sectionList = document.getElementById(section);
+        sectionList.innerHTML = '';
+        data.forEach((book, index) => {
+            const bookElement = document.createElement('div');
+            bookElement.className = 'item';
+            const uniqueParam = new Date().getTime() + index;
+            bookElement.innerHTML = `
+                <img src="${book.title_image}?random=${uniqueParam}" alt="${book.title}">
+                <h3>${book.title}</h3>
+            `;
+            bookElement.addEventListener('click', () => openModal(book));
+            sectionList.appendChild(bookElement);
+        });
     })
     .catch(error => console.error('Error loading books:', error));
+}
+
+function openModal(book) {
+    document.getElementById('modal-title').textContent = book.title;
+    document.getElementById('modal-cover').src = book.title_image;
+    document.getElementById('modal-author').textContent = book.author;
+    document.getElementById('modal-genre').textContent = book.genre;
+    document.getElementById('modal-pages').textContent = book.pages;
+    document.getElementById('modal-release-date').textContent = new Date(book.release_date).toDateString();
+
+    const modal = document.getElementById('book-modal');
+    modal.style.display = 'block';
+
+    const closeButton = document.querySelector('.close-button');
+    closeButton.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
 }
